3dfd523cfcee5dabb50a7486e1f93d35df83233b
eoghankb <eoghankb@gmail.com>
2022-11-03 15:24:09 +0000

    Add new url paths for other nav links

--- home/urls.py
+++ home/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='home')
+    path('', views.index, name='home'),
+    path('about/', views.index, name='about'),
+    path('plans/', views.index, name='plans'),
+    path('prices/', views.index, name='prices')
 ]
