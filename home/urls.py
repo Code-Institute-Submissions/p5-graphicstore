f80f308dec7bfe8d701f54f94a388dcec3b0b222
eoghankb <eoghankb@gmail.com>
2022-11-04 13:42:47 +0000

    Changed views.index to the right page names

--- home/urls.py
+++ home/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='home'),
-    path('about/', views.index, name='about'),
-    path('plans/', views.index, name='plans'),
-    path('prices/', views.index, name='prices')
+    path('', views.index, name='index'),
+    path('about/', views.about, name='about'),
+    path('plans/', views.plans, name='plans'),
+    path('prices/', views.prices, name='prices')
 ]
