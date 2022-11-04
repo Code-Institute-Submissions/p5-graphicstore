fe9478495d0f0b54c646489d79636ee76a0be3cd
eoghankb <eoghankb@gmail.com>
2022-11-04 13:43:05 +0000

    Added views to render individual pages

--- home/views.py
+++ home/views.py
@@ -4,3 +4,18 @@ from django.shortcuts import render
 def index(request): 
     """ A view to return the index page"""
     return render(request, 'home/index.html')
+
+
+def about(request): 
+    """ A view to return the about page"""
+    return render(request, 'home/about.html')
+
+
+def plans(request): 
+    """ A view to return the about page"""
+    return render(request, 'home/plans.html')
+
+
+def prices(request): 
+    """ A view to return the about page"""
+    return render(request, 'home/prices.html')
