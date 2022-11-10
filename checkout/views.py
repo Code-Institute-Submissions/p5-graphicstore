d32803baf0062e5a81088fe4d26f7b06187eb367
eoghankb <eoghankb@gmail.com>
2022-11-10 20:04:53 +0000

    Added underscore for orderform

--- checkout/views.py
+++ checkout/views.py
@@ -3,16 +3,14 @@ from django.contrib import messages
 
 from .forms import OrderForm
 
-# Create your views here.
-
 
 def checkout(request):
     bag = request.session.get('bag', {})
     if not bag:
         messages.error(request, "There's nothing in your bag at the moment")
-        return redirect(reversed('products'))
-    
-    order_form = Order_form()
+        return redirect(reverse('products'))
+
+    order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
