8f909bc32f76bd359653794d9b4be9e73ea410e9
eoghankb <eoghankb@gmail.com>
2022-11-14 16:17:21 +0000

    added warning message for public key

--- checkout/views.py
+++ checkout/views.py
@@ -20,20 +20,22 @@ def checkout(request):
     current_bag = bag_contents(request)
     total = current_bag['grand_total']
     stripe_total = round(total * 100)
-    stripe_api_key = settings.STRIPE_SECRET_KEY
+    stripe.api_key = settings.STRIPE_SECRET_KEY
     intent = stripe.PaymentIntent.create(
         amount=stripe_total,
-        currency=settings.STRIPE_CURRENCY
+        currency=settings.STRIPE_CURRENCY,
     )
 
-    print(intent)
-
     order_form = OrderForm()
+
+    if not stripe_public_key:
+        messages.warning(request, 'Stripe public key is missing. Did you forget to set it in your enviroment?')
+
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51M2iSCFQHM7sSprWp84DTcILSyunjMCd1inIUSvKx6AG1qPut2uIOfNilCh6FpZfFEsn99C1dboeHyZEPpgXMgyZ002PnTtH05',
-        'client_secret': 'test_client_secert',
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
