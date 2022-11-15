fb68e761058b7f47d9a518329b11a84f07c909fa
eoghankb <eoghankb@gmail.com>
2022-11-15 20:07:27 +0000

    removed plans and prices url paths

--- home/urls.py
+++ home/urls.py
@@ -4,6 +4,4 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
-    path('plans/', views.plans, name='plans'),
-    path('prices/', views.prices, name='prices')
 ]
