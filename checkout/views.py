2049a339909d7789ecb4b28180a192219f670266
eoghankb <eoghankb@gmail.com>
2022-11-18 05:30:06 +0000

    fixed spelling mistake

--- checkout/views.py
+++ checkout/views.py
@@ -14,14 +14,14 @@ import os
 
 
 @require_POST
-def cache_checkout_data():
+def cache_checkout_data(request):
     try:
-        pid = require.POST.get('client_secret')
+        pid = request.POST.get('client_secret').split('_secret')[0]
         stripe_secret_key = os.environ.get("STRIPE_SECRET_KEY")
         stripe.PaymentIntent.modify(pid, metadata={
             'bag': json.dumps(request.session.get('bag', {})),
-            'save_info': require_POST.get('save_info'),
-            'username': redirect.user,
+            'save_info': request.POST.get('save_info'),
+            'username': request.user,
         })
         return HttpResponse(status=200)
     except Exception as e:
@@ -50,7 +50,11 @@ def checkout(request):
         }
         order_form = OrderForm(form_data)
         if order_form.is_valid():
-            order = order_form.save()
+            order = order_form.save(commit=False)
+            pid = request.POST.get('client_secret').split('_secret')[0]
+            order.stripe_pid = pid
+            order.origin_bag = json.dumps(bag)
+            order.save()
             for item_id, item_data in bag.items():
                 try:
                     product = Product.objects.get(id=item_id)
