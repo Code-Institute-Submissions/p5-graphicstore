4297f1aa1a206858b7fad34172be6d0223f0c913
eoghankb <eoghankb@gmail.com>
2022-11-19 22:42:43 +0000

    added import for UserProfile

--- profiles/views.py
+++ profiles/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
 
 
+
 # Create your views here.
 def profile(request):
     """Display the user's profile"""
