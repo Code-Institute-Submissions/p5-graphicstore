8a7956ed9bb44a7c16e54c5ebd3f81669c7e0568
eoghankb <eoghankb@gmail.com>
2022-11-21 09:11:01 +0000

    changed added graphi-cstore to allowed hosts

--- graphicstore/settings.py
+++ graphicstore/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["app-graphicstore.herokuapp.com","localhost"]
+ALLOWED_HOSTS = ["graphic-store.heroku.com", "localhost"]
 
 
 # Application definition
