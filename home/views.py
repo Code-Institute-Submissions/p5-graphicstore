0871b862afeb5ccc7f85aad43f2ce380f104014c
eoghankb <eoghankb@gmail.com>
2022-11-29 16:31:31 +0000

    added context for news letter form and function and subscribe function

--- home/views.py
+++ home/views.py
@@ -6,9 +6,22 @@ from django.shortcuts import redirect
 
 def index(request): 
     """ A view to return the index page"""
-    return render(request, 'home/index.html')
+    form = NewsletterForm()
+    context = { 
+        'form': form,
+    }
+
+    return render(request, 'home/index.html', context)
 
 
 def about(request): 
     """ A view to return the about page"""
     return render(request, 'home/about.html')
+
+
+def subscribe(request):
+    """ """
+    form = NewsletterForm(request.POST)
+    if form.is_valid():
+        form.save()
+    return redirect('index')
