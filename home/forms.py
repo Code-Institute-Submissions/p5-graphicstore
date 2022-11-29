37856d16a4c95cb8aa1ab692cb4e4bb944fa745c
eoghankb <eoghankb@gmail.com>
2022-11-29 16:24:57 +0000

    added logic for news letter form

---
+++ home/forms.py
@@ -0,0 +1,22 @@
+from django import forms
+from .models import Newsletter
+
+
+class NewsletterForm(forms.ModelForm):
+    class Meta:
+        model = Newsletter
+        fields = ['email'] 
+    
+    def __init__(self, *args, **kwargs):
+        """
+        Add placeholders and classes, remove auto-generated
+        labels and set autofocus on first field
+        """
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'email': 'Email',
+        }
+
+        for field in self.fields:
+            self.fields[field].widget.attrs['class'] = 'border-black rounded-0 newsletter-form-input'
+            self.fields[field].label = False
