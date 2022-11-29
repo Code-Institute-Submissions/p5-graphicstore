83adfb84d78c155352f476b6b19f1337dcf76a54
eoghankb <eoghankb@gmail.com>
2022-11-29 16:24:32 +0000

    added custom model newsletter on admin page

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Newsletter
 
 # Register your models here.
+
+
+class NewsletterAdmin(admin.ModelAdmin):
+    readonly_fields = ('email', 'date',)
+
+    fields = ('date','email',)
+
+    list_display = ('email', 'date',)
+
+    ordering = ('-date',)
+
+
+admin.site.register(Newsletter, NewsletterAdmin)
