9c60c4e1f1087b74484ae9d872d429b1ec8ef4f3
eoghankb <eoghankb@gmail.com>
2022-11-30 22:04:27 +0000

    added first custom model newsletter

--- home/models.py
+++ home/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
-# Create your models here.
+# Custom model number 1
 
 
-class Newsletter(models.Model): 
+class Newsletter(models.Model):
     email = models.EmailField(max_length=254, null=False, blank=False)
     date = models.DateTimeField(auto_now_add=True)
